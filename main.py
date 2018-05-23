028932194c56210cf8b75042d921314ebcfef95b
M <lmiguelvargasf@gmail.com>
2018-05-23 00:30:49 -0500

    Move show_clients function to show_clients file

--- main.py
+++ main.py
@@ -1,33 +1,7 @@
 import tkinter as tk
 
-from os import listdir
-from os.path import isfile, join
-
-
 from new_client import add_new_client
-
-
-def show_clients():
-    clients = tk.Toplevel()
-    clients.title('Clients')
-
-    label_clients = tk.Label(clients, text='Clients')
-    label_clients.pack(fill=tk.X)
-
-    records_path = 'clients/'
-    records = [f for f in listdir(records_path) if isfile(join(records_path, f))]
-
-    for record in records:
-        with open(join(records_path, record)) as file:
-            name = file.readline().strip()[6:]
-            last_name = file.readline().strip()[11:]
-            client_id = file.readline().strip()[4:]
-            client_class = file.readline().strip()[11:]
-
-        client_label = tk.Label(
-            clients,
-            text=f'{client_id} - {name} - {last_name} - {client_class}')
-        client_label.pack(fill=tk.X)
+from show_clients import show_clients
 
 
 def main():
@@ -46,6 +20,7 @@ def main():
     show_clients_button.pack()
 
     root.mainloop()
+
 
 if __name__ == '__main__':
     main()
---
+++ show_clients.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+
+from os import listdir
+from os.path import isfile, join
+
+
+def show_clients():
+    clients = tk.Toplevel()
+    clients.title('Clients')
+
+    label_clients = tk.Label(clients, text='Clients')
+    label_clients.pack(fill=tk.X)
+
+    records_path = 'clients/'
+    records = [f for f in listdir(records_path) if isfile(join(records_path, f))]
+
+    for record in records:
+        with open(join(records_path, record)) as file:
+            name = file.readline().strip()[6:]
+            last_name = file.readline().strip()[11:]
+            client_id = file.readline().strip()[4:]
+            client_class = file.readline().strip()[11:]
+
+        client_label = tk.Label(
+            clients,
+            text=f'{client_id} - {name} - {last_name} - {client_class}')
+        client_label.pack(fill=tk.X)
