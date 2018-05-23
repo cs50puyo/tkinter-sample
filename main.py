5dc07c684926b16415cef51b5349361ce6a2641d
M <lmiguelvargasf@gmail.com>
2018-05-23 00:28:01 -0500

    Display information of each record on clients window

--- main.py
+++ main.py
@@ -17,6 +17,19 @@ def show_clients():
     records_path = 'clients/'
     records = [f for f in listdir(records_path) if isfile(join(records_path, f))]
 
+    for record in records:
+        with open(join(records_path, record)) as file:
+            name = file.readline().strip()[6:]
+            last_name = file.readline().strip()[11:]
+            client_id = file.readline().strip()[4:]
+            client_class = file.readline().strip()[11:]
+
+        client_label = tk.Label(
+            clients,
+            text=f'{client_id} - {name} - {last_name} - {client_class}')
+        client_label.pack(fill=tk.X)
+
+
 def main():
     root = tk.Tk()
     root.title('Bar register')
