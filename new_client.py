817fb0da15e47fef3a7d47711433e20dc4fef13d
M <lmiguelvargasf@gmail.com>
2018-05-22 23:56:54 -0500

    Implement create_new_client function and pass it though lambda
    Implement function to create a new client record and use a lambda to pass a function that takes arguments as a command.

--- new_client.py
+++ new_client.py
@@ -1,7 +1,15 @@
 import tkinter as tk
 
-def create_new_client():
-    pass
+
+def create_new_client(id, name, last_name, classroom, window):
+    unique_name = f'{id}_{last_name}_{name}.txt'
+    with open(unique_name, 'w') as new_record:
+        new_record.write(f'Name: {name}\n')
+        new_record.write(f'Last Name: {last_name}\n')
+        new_record.write(f'Id: {id}\n')
+        new_record.write(f'Classroom: {classroom}\n\n')
+
+    window.destroy()
 
 
 def add_new_client():
@@ -30,14 +38,19 @@ def add_new_client():
     class_label.grid(row=3, column=0)
 
     CLASSROOMS = ('1A', '1B', '1C', '1D',
-               '2A', '2B', '2C', '2D', '1IB',
-               '3A', '3B', '3C', '2IB')
+                  '2A', '2B', '2C', '2D', '1IB',
+                  '3A', '3B', '3C', '2IB')
     classroom_var = tk.StringVar()
     classroom_var.set(CLASSROOMS[0])
     class_option_menu = tk.OptionMenu(new_client, classroom_var, *CLASSROOMS)
     class_option_menu.grid(row=3, column=1)
 
-    new_client_button = tk.Button(new_client,
+    new_client_button = tk.Button(
+        new_client,
         text='Create New Client',
-                                  command=create_new_client)
+        command=lambda: create_new_client(id_var.get(),
+                                          name_var.get(),
+                                          last_name_var.get(),
+                                          classroom_var.get(),
+                                          new_client))
     new_client_button.grid(row=4, column=1)
