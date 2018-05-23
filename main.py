2af3d445224e1f3afb88c9d2efb87a92d19c3e0d
M <lmiguelvargasf@gmail.com>
2018-05-22 20:54:10 -0500

    Add buttons to create new record and to show clients

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import tkinter as tk
 
 def main():
     root = tk.Tk()
+
+    new_record_button = tk.Button(root, text='New Record')
+    new_record_button.pack()
+    show_clients_button = tk.Button(root, text='Show Clients')
+    show_clients_button.pack()
+
     root.mainloop()
 
 if __name__ == '__main__':
