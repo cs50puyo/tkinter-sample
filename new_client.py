1a078a35b4bcf13428f0b284f36175000231a0c7
M <lmiguelvargasf@gmail.com>
2018-05-22 23:40:39 -0500

    Rename variables

--- new_client.py
+++ new_client.py
@@ -26,15 +26,15 @@ def add_new_client():
     class_label = tk.Label(new_client, text='Class')
     class_label.grid(row=3, column=0)
 
-    CLASSES = ('1A', '1B', '1C', '1D',
+    CLASSROOMS = ('1A', '1B', '1C', '1D',
                '2A', '2B', '2C', '2D', '1IB',
                '3A', '3B', '3C', '2IB')
-    selected_class = tk.StringVar()
-    selected_class.set(CLASSES[0])
-    class_option_menu = tk.OptionMenu(new_client, selected_class, *CLASSES)
+    classroom_var = tk.StringVar()
+    classroom_var.set(CLASSROOMS[0])
+    class_option_menu = tk.OptionMenu(new_client, classroom_var, *CLASSROOMS)
     class_option_menu.grid(row=3, column=1)
 
     new_client_button = tk.Button(new_client,
-                                  text='Create New Client',
+        text='Create New Client',
                                   command=create_new_client)
     new_client_button.grid(row=4, column=1)
