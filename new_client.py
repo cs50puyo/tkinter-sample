c236f20dd6ac3e11e3a766b619b14aade38a3f33
M <lmiguelvargasf@gmail.com>
2018-05-24 16:41:21 -0500

    Remove needless variables

--- new_client.py
+++ new_client.py
@@ -18,20 +18,17 @@ def add_new_client():
 
     name_label = tk.Label(new_client, text='Name')
     name_label.grid(row=0, column=0)
-    name_var = tk.StringVar()
-    name_entry = tk.Entry(new_client, textvariable=name_var)
+    name_entry = tk.Entry(new_client)
     name_entry.grid(row=0, column=1)
 
     last_name_label = tk.Label(new_client, text='Last Name')
     last_name_label.grid(row=1, column=0)
-    last_name_var = tk.StringVar()
-    last_name_entry = tk.Entry(new_client, textvariable=last_name_var)
+    last_name_entry = tk.Entry(new_client)
     last_name_entry.grid(row=1, column=1)
 
     id_label = tk.Label(new_client, text='Id')
     id_label.grid(row=2, column=0)
-    id_var = tk.StringVar()
-    id_entry = tk.Entry(new_client, textvariable=id_var)
+    id_entry = tk.Entry(new_client)
     id_entry.grid(row=2, column=1)
 
     class_label = tk.Label(new_client, text='Class')
@@ -48,9 +45,9 @@ def add_new_client():
     new_client_button = tk.Button(
         new_client,
         text='Create New Client',
-        command=lambda: create_new_client(id_var.get(),
-                                          name_var.get(),
-                                          last_name_var.get(),
+        command=lambda: create_new_client(id_entry.get(),
+                                          name_entry.get(),
+                                          last_name_entry.get(),
                                           classroom_var.get(),
                                           new_client))
     new_client_button.grid(row=4, column=1)
